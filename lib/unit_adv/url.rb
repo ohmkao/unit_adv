5653c85ef76c030c2eecd2f96fa9f36697dab75d
ohm <ohm@picowork.com>
2017-04-07 19:46:50 +0800

    fix

--- lib/unit_adv/url.rb
+++ lib/unit_adv/url.rb
@@ -3,28 +3,28 @@
 
 class UnitAdv::Url
 
-  attr_accessor :obj
+  #attr_accessor :obj
 
-  def initialize(init_url)
-    self.obj = URI.parse(init_url)
-  end
+  #def initialize(init_url)
+  #  self.obj = URI.parse(init_url)
+  #end
 
-  def query_hash
-    return CGI.parse obj.query if obj.query.present?
-    nil
-  end
+  #def query_hash
+  #  return CGI.parse obj.query if obj.query.present?
+  #  nil
+  #end
 
-  def subdomain(n = 0)
-    n = -3 if n == "max"
-    obj.host.split('.')[0..n].join('.')
-  end
+  #def subdomain(n = 0)
+  #  n = -3 if n == "max"
+  #  obj.host.split('.')[0..n].join('.')
+  #end
 
-  def domain(path = "/")
-    URI.join obj.to_s, path
-  end
+  #def domain(path = "/")
+  #  URI.join obj.to_s, path
+  #end
 
-  def method_missing(method_sym)
-    obj.send(method_sym)
-  end
+  #def method_missing(method_sym)
+  #  obj.send(method_sym)
+  #end
 
 end
